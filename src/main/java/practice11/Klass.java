2d68c31f9acdcc747a06482b2200b4a26dcd924f
Czkl <zw407461270@163.com>
2019-03-27 14:46:36 +0800

    complete practice11

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,59 @@
 package practice11;
 
-public class Klass {
+
+
+import java.util.List;
+import java.util.Observable;
+
+public class Klass extends Observable {
+    private Integer number;
+
+    private Student leader;
+
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getDisplayName(){
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student){
+        if (student.getKlass().getNumber() != this.getNumber()) {
+            System.out.print("It is not one of us.\n");
+            return;
+        }
+        this.leader = student;
+        setChanged();
+        notifyObservers(this);
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+        setChanged();
+        notifyObservers(student);
+    }
+
+    public boolean isIn(List<Klass> classes) {
+        if (classes == null || classes.isEmpty()) {
+            return false;
+        } else {
+            return classes.contains(this);
+        }
+    }
+
 }
+
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,77 @@
 package practice11;
 
-public class Teacher {
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Teacher extends Person implements Observer {
+
+    private List<Klass> classes;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, List<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public Teacher(String name, int age, List<Klass> classes) {
+        super(name, age);
+        this.classes = classes;
+    }
+
+    public List<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(List<Klass> classes) {
+        this.classes = classes;
+    }
+
+    @Override
+    public String introduce() {
+        if (classes == null || classes.size() == 0) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            String str = super.introduce() + " I am a Teacher. I teach Class ";
+            for (Klass aClass : classes) {
+                str += aClass.getNumber() +", ";
+            }
+            str = str.trim().substring(0, str.length() - 2) +".";
+            return str;
+        }
+    }
+    public String introduceWith(Student student){
+        if (isTeaching(student)){
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        } else {
+            return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+        }
+    }
+
+    public boolean isTeaching(Student student){
+        if (classes == null || classes.isEmpty()) {
+            return false;
+        } else {
+            return student.getKlass().isIn(classes);
+        }
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if (arg instanceof Klass) {
+            Klass klass = (Klass) o;
+            if (classes.contains(klass)){
+                System.out.print("I am "+this.getName()+". I know "+klass.getLeader().getName()+" become Leader of Class "+klass.getNumber()+".\n");
+            }
+        }else if(arg instanceof Student){
+            Student student = (Student) arg;
+            Klass klass = student.getKlass();
+            System.out.print("I am "+this.getName()+". I know "+student.getName()+" has joined Class "+klass.getNumber()+".\n");
+        }
+    }
 }
+
