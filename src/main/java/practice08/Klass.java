fe0d5c3f84cdc4bc055ef23812aed1a50572134c
Czkl <zw407461270@163.com>
2019-03-27 14:31:13 +0800

    complete practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,31 @@
 package practice08;
 
 public class Klass {
+    private Integer number;
+
+    private Student leader;
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getDisplayName(){
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student){
+        this.leader = student;
+    }
 }
