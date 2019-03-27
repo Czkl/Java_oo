47e117854a48ce8852161c1fbe0bc82874e1cd2c
Czkl <zw407461270@163.com>
2019-03-27 14:29:32 +0800

    complete practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,22 @@
 package practice07;
 
 public class Klass {
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    private Integer number;
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getDisplayName(){
+        return "Class " + number;
+    }
 }
